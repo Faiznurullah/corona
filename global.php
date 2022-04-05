242053fabbb8e997a8617e8b78e8b4d7972b3076
Grakody <59213454+Faiznurullah@users.noreply.github.com>
2022-04-05 15:36:32 +0700

    add file

---
+++ global.php
@@ -0,0 +1,331 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+   <link rel="icon"  a href="img/virus.png" type="image/gif" sizes="16x16">
+
+  <title>Info Covid-19</title>
+
+  <!-- Custom fonts for this template-->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+  <!-- Custom styles for this template-->
+  <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
+
+
+</head>
+
+<body id="page-top">
+
+  <!-- Page Wrapper -->
+  <div id="wrapper">
+
+    <!-- Sidebar -->
+    <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+      <!-- Sidebar - Brand -->
+      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
+        <div class="sidebar-brand-icon rotate-n-15">
+          <i class="fas fa-hospital"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">Info Covid-19</div>
+      </a>
+
+      <!-- Divider -->
+      <hr class="sidebar-divider my-0">
+
+      <!-- Nav Item - Dashboard -->
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">
+          <i class="fas fa-fw fa-home"></i>
+          <span>Home</span></a>
+      </li>
+
+      <!-- Divider -->
+      <hr class="sidebar-divider">
+
+      <!-- Heading -->
+      <div class="sidebar-heading">
+        Covid-19
+      </div>
+
+      <!-- Nav Item - Pages Collapse Menu -->
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+          <i class="fas fa-fw fa-file"></i>
+          <span>Data Covid-19</span>
+        </a>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Live Data:</h6>
+            <a class="collapse-item" href="indo.php">Indonesia</a>
+            <a class="collapse-item" href="global.php">Dunia (Global)</a>
+          </div>
+        </div>
+      </li>
+
+      <!-- Nav Item - Utilities Collapse Menu -->
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
+          <i class="fas fa-fw fa-building"></i>
+          <span>Developers</span>
+        </a>
+        <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Developers Tools:</h6>
+            <a class="collapse-item" href="api.php">Sumber API</a>
+          </div>
+        </div>
+      </li>
+
+
+
+      <!-- Divider -->
+      <hr class="sidebar-divider d-none d-md-block">
+
+      <!-- Sidebar Toggler (Sidebar) -->
+      <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+      </div>
+
+    </ul>
+    <!-- End of Sidebar -->
+
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+
+      <!-- Main Content -->
+      <div id="content">
+
+        <!-- Topbar -->
+        <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+
+          <!-- Sidebar Toggle (Topbar) -->
+          <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+            <i class="fa fa-bars"></i>
+          </button>
+
+
+
+
+
+        </nav>
+        <!-- End of Topbar -->
+
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
+<center><h2 class="mb-5">Live Data Covid-19 Dunia</h2></center>
+
+  <!--ini content-->
+          <div class="row">
+
+
+             <div class="col-lg-4">
+               <div class="card shadow mb-4">
+                 <div class="card-body bg-danger">
+                   <div class="row">
+                     <div class="col-md-7 text-white">
+                       <h5>Positif</h5>
+                       <h2 id="data-kasus">123</h2>
+                       <h5>Orang</h5>
+                     </div>
+                     <div class="col-md-4">
+                     <img src="img/sakit.png" style="width: 100px;">
+                    </div>
+                   </div>
+               </div>
+               </div>
+             </div>
+
+             <div class="col-lg-4">
+               <div class="card shadow mb-4">
+                 <div class="card-body bg-warning">
+                   <div class="row">
+                     <div class="col-md-7 text-white">
+                       <h5>Meninggal</h5>
+                       <h2 id="data-mati">123</h2>
+                       <h5>Orang</h5>
+                     </div>
+                     <div class="col-md-4">
+                  <img src="img/meninggal.png" style="width: 100px;">
+                    </div>
+                   </div>
+               </div>
+               </div>
+             </div>
+
+             <div class="col-lg-4">
+               <div class="card shadow mb-4">
+                 <div class="card-body bg-success">
+                   <div class="row">
+                     <div class="col-md-7 text-white">
+                       <h5>Sembuh</h5>
+                       <h2 id="data-sembuh">123</h2>
+                       <h5>Orang</h5>
+                     </div>
+                     <div class="col-md-4">
+               <img src="img/sehat.png" style="width: 100px;">
+                    </div>
+                   </div>
+               </div>
+               </div>
+             </div>
+
+
+          </div>
+
+<!--row2-->
+          <div class="row">
+              <div class="col-xl-1 col-lg-8">
+              </div>
+
+              <div class="col-xl-10 col-lg-8">
+            <div class="card shadow mb-4">
+              <!-- Card Header - Dropdown -->
+              <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                <h6 class="m-0 font-weight-bold text-primary">Data Table Global:</h6>
+              </div>
+              <!-- Card Body -->
+              <div class="card-body">
+                <div class="table-responsive service">
+                <table class="table table-bordered table-hover  mt-3 text-nowrap css-serial">
+                <thead>
+                <th>No</th>
+                <th>Nama Negara</th>
+                <th>Positif</th>
+                <th>Sembuh</th>
+                <th>Meninggal</th>
+                </thead>
+                <tbody id="data-global">
+
+
+                </tbody>
+                </table>
+              </div>
+
+
+              </div>
+            </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+
+          <!-- Footer -->
+          <footer class="sticky-footer bg-white">
+            <div class="container my-auto">
+              <div class="copyright text-center my-auto">
+                <span><p class="mb-1">Copyright &copy; <a href="https://www.dicoding.com/challenges/619" style="text-decoration: none;"><b>Dicoding Challange</b></a></p></span><br>
+    </div>
+            </div>
+          </footer>
+          <!-- End of Footer -->
+
+
+    </div>
+      <!-- End of Main Content -->
+
+
+
+    </div>
+    <!-- End of Content Wrapper -->
+
+  </div>
+  <!-- End of Page Wrapper -->
+
+  <!-- Scroll to Top Button-->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+
+
+
+  <!-- Bootstrap core JavaScript-->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="js/sb-admin-2.min.js"></script>
+
+
+
+  <script>
+    $(document).ready(function(){
+
+  //panggil semua fungsi data global //
+
+
+    semuaData();
+
+    dataDunia();
+
+
+
+    function semuaData(){
+      $.ajax({
+      url : 'https://coronavirus-19-api.herokuapp.com/all',
+      success : function(data){
+        try{
+    	var json = data;
+    	var kasus = data.cases;
+    	var meninggal = data.deaths;
+    	var sembuh = data.recovered;
+
+    	$('#data-kasus').html(kasus);
+    	$('#data-mati').html(meninggal);
+    	$('#data-sembuh').html(sembuh);
+
+
+    	}catch{
+    	alert('eror kang sistemna');
+    	}
+      }
+      });
+    }
+
+
+
+
+
+     function dataDunia(){
+     $.ajax({
+      url : 'curl2.php',
+      type : 'GET',
+      success : function(data){
+        try{
+      $('#data-global').html(data);
+    	}catch{
+    	alert('eror kang sistemna');
+    	}
+      }
+      });
+
+
+     }
+
+
+
+
+  });
+
+  </script>
+
+
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,279 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+   <link rel="icon"  a href="img/virus.png" type="image/gif" sizes="16x16">
+
+  <title>Info Covid-19</title>
+
+  <!-- Custom fonts for this template-->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+  <!-- Custom styles for this template-->
+  <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
+
+
+</head>
+
+<body id="page-top">
+
+  <!-- Page Wrapper -->
+  <div id="wrapper">
+
+    <!-- Sidebar -->
+    <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+      <!-- Sidebar - Brand -->
+      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
+        <div class="sidebar-brand-icon rotate-n-15">
+          <i class="fas fa-hospital"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">Info Covid-19</div>
+      </a>
+
+      <!-- Divider -->
+      <hr class="sidebar-divider my-0">
+
+      <!-- Nav Item - Dashboard -->
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">
+          <i class="fas fa-fw fa-home"></i>
+          <span>Home</span></a>
+      </li>
+
+      <!-- Divider -->
+      <hr class="sidebar-divider">
+
+      <!-- Heading -->
+      <div class="sidebar-heading">
+        Covid-19
+      </div>
+
+      <!-- Nav Item - Pages Collapse Menu -->
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+          <i class="fas fa-fw fa-file"></i>
+          <span>Data Covid-19</span>
+        </a>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Live Data:</h6>
+            <a class="collapse-item" href="indo.php">Indonesia</a>
+            <a class="collapse-item" href="global.php">Dunia (Global)</a>
+          </div>
+        </div>
+      </li>
+
+      <!-- Nav Item - Utilities Collapse Menu -->
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
+          <i class="fas fa-fw fa-building"></i>
+          <span>Developers</span>
+        </a>
+        <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Developers Tools:</h6>
+            <a class="collapse-item" href="api.php">Sumber API</a>
+          </div>
+        </div>
+      </li>
+
+
+
+      <!-- Divider -->
+      <hr class="sidebar-divider d-none d-md-block">
+
+      <!-- Sidebar Toggler (Sidebar) -->
+      <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+      </div>
+
+    </ul>
+    <!-- End of Sidebar -->
+
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+
+      <!-- Main Content -->
+      <div id="content">
+
+        <!-- Topbar -->
+        <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+
+          <!-- Sidebar Toggle (Topbar) -->
+          <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+            <i class="fa fa-bars"></i>
+          </button>
+
+
+
+
+
+        </nav>
+        <!-- End of Topbar -->
+
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
+
+
+          <!-- Content Row -->
+
+          <div class="row">
+<div class="col-xl-1">
+
+</div>
+
+
+
+            <div class="col-xl-10 col-lg-8">
+              <div class="card shadow mb-4">
+                <!-- Card Header - Dropdown -->
+                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+
+                </div>
+                <!-- Card Body -->
+                <div class="card-body">
+                  <center><img class="mt-2" src="img/covid-19.png" alt="Virus" width="100px" height="100px"></center>
+                  <center><h5 class="mt-5"><strong>Info Covid-19</strong></h5></center>
+                  <center><b>"Yuk, Pantau Secara Seksama Live Data Covid-19 Di Dunia Maupun Indonesia"</b></center>
+
+                </div>
+              </div>
+            </div>
+
+
+      </div>
+
+      <center><h3 class="mt-5">Mengenal Covid</h3></center>
+
+<!--Row ke1-->
+      <div class="row">
+
+        <div class="col-xl-1">
+        </div>
+
+
+        <div class="col-xl-5 col-lg-4 mb-3">
+          <div class="card bg-danger text-white shadow">
+            <div class="card-body">
+              <a href="https://sehatnegeriku.kemkes.go.id/baca/rilis-media/20200316/4033408/lakukan-protokol-kesehatan-jika-mengalami-gejala-covid-19/" style="text-decoration: none;">
+                <div class="card-body bg-danger">
+           <b class="text-white">Lakukan Protokol Kesehatan ini jika Mengalami Gejala Covid-19</b><br><br>
+              </div>
+            </a>
+              <div class="text-white-80 small">Kementrian Kesehatan</div>
+            </div>
+          </div>
+
+        </div>
+
+        <div class="col-xl-5 col-lg-4 mb-3">
+          <div class="card bg-warning text-white shadow">
+            <div class="card-body">
+              <a href="https://sehatnegeriku.kemkes.go.id/baca/rilis-media/20200331/5133667/cara-cegah-penularan-covid-19-masyarakat/" style="text-decoration: none;">
+                <div class="card-body bg-warning">
+           <b class="text-white">Cara Cegah Penularan Covid-19 di Masyarakat Indonesia</b><br><br>
+              </div>
+            </a>
+              <div class="text-white-80 small">Kementrian Kesehatan</div>
+            </div>
+          </div>
+
+        </div>
+
+
+      </div>
+
+<!--Row ke2-->
+      <div class="row">
+
+        <div class="col-xl-1">
+        </div>
+
+        <div class="col-xl-5 col-lg-4 mb-3">
+          <div class="card bg-success text-white shadow">
+            <div class="card-body">
+              <a href="https://www.kompas.com/tren/read/2020/03/03/183500265/infografik-daftar-100-rumah-sakit-rujukan-penanganan-virus-corona" style="text-decoration: none;">
+                <div class="card-body bg-success">
+           <b class="text-white">Daftar Rumah Sakit rujukan penanganan virus covid-19</b><br><br>
+              </div>
+            </a>
+              <div class="text-white-80 small">Kompas</div>
+            </div>
+          </div>
+
+        </div>
+
+        <div class="col-xl-5 col-lg-4 mb-3">
+          <div class="card bg-info text-white shadow">
+            <div class="card-body">
+              <a href="https://health.kompas.com/read/2020/05/30/120000968/lakukan-6-kebiasaan-ini-jika-ingin-pandemi-covid-19-cepat-selesai?page=all" style="text-decoration: none;">
+                <div class="card-body bg-info">
+              <b class="text-white">Lakukan Kebiasaan Ini jika Ingin Pandemi Covid-19 Cepat Selesai</b><br><br>
+              </div>
+            </a>
+              <div class="text-white-80 small">Kompas</div>
+            </div>
+          </div>
+
+        </div>
+
+
+
+
+      </div>
+
+      <!-- Footer -->
+      <footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span><p class="mb-1">Copyright &copy; <a href="https://www.dicoding.com/challenges/619" style="text-decoration: none;"><b>Dicoding Challange</b></a></p></span>
+          </div>
+        </div>
+      </footer>
+      <!-- End of Footer -->
+
+
+    </div>
+      <!-- End of Main Content -->
+
+
+
+    </div>
+    <!-- End of Content Wrapper -->
+
+  </div>
+  <!-- End of Page Wrapper -->
+
+  <!-- Scroll to Top Button-->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+
+
+
+  <!-- Bootstrap core JavaScript-->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="js/sb-admin-2.min.js"></script>
+
+
+
+
+
+</body>
+
+</html>
